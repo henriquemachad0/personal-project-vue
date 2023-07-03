8200b52c93689f91fbb3b5d2d462775590612ebe
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-07-03 14:58:52 -0300

    feat: add loadProducts in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,20 @@
 import { createStore } from "vuex";
+import axios from 'axios'
 
 export default createStore({
-    state:{},
-    mutations:{},
-    actions:{}
+    state:{
+        products: []
+    },
+    mutations:{
+        loadProducts(state, products){
+            state.products = products
+        }
+    },
+    actions:{
+        loadProducts({commit}) {
+            axios.get('https://fakestoreapi.com/products').then(response => {
+                commit('loadProducts', response.data)
+            })
+        }
+    }
 })
