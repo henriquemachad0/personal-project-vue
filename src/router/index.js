08b2f60000ae9d3efc95c123b82e31d1683cfa5a
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-07-04 17:05:24 -0300

    feat: add router product page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import Home from '../views/HomePage.vue'
+import Product from '../views/ProductPage.vue'
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/:id',
+        name: 'Product',
+        component: Product
     }
 ]
 
